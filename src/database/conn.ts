9c420636c2cbb0b6f7a01c877583eed8dde76c1c
Aditya Khattri <adityakhattri123@gmail.com>
2023-11-09 18:46:22 +0530

    Added env variables

--- src/database/conn.ts
+++ src/database/conn.ts
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
+import {db} from "./config"
 
-const connectionString = 'mongodb+srv://adityakhattri112233:aditya112233@cluster0.kpr4bi4.mongodb.net/?retryWrites=true&w=majority';//added since not working from env but will find a solution for this
-
+const connectionString = process.env.MONGO_URI || '';//added since not working from env but will find a solution for this
 mongoose.connect(connectionString)
 .then(()=>{
     console.log('Connected with database');
 })
 .catch(()=>{
     console.log("Error in connecting with database . Exiting.....");
+    console.log(connectionString)
     process.exit(0);
 })
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express,{Application,Request,Response} from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import playerRoute from './routes/playerRoute';
+import './database/conn';
+
+const app:Application = express();
+const port = process.env.PORT || 3000; 
+app.use(express.json());
+
+app.use('/',playerRoute);
+app.get("/",(req:Request,res:Response)=>{ 
+    res.status(200).json({message:"Working"});
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is up at ${port}`);
+})
